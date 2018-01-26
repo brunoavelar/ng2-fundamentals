6990ad29f8e4534f7ab02c7734136e00112e3866
Bruno Avelar <brunoavelar@gmail.com>
2018-01-26 16:55:48 -0500

    Add routing

---
+++ app/events/event-details/event-details.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../shared/events.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    templateUrl: '/app/events/event-details/event-details.component.html',
+    styles: [`
+        .container { padding-left: 20px; pedding-right: 20px; }
+        .event-image { height: 100px; }
+    `]
+})
+
+export class EventDetailsComponent implements OnInit {
+    event:any;
+
+    constructor(private eventService: EventService, private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        let id:number = +this.route.snapshot.params['id'];
+        this.event = this.eventService.getEvent(id);
+     }
+}
